a847157b38be5c03c5d9b4f540e9d6933155cfb6
HTHKDOM\bobbyfyf <bobby.fan@hthk.com>
2025-12-18 11:01:02 +0800

    no message

--- src/components/pages/game/Game.tsx
+++ src/components/pages/game/Game.tsx
@@ -466,10 +466,7 @@ const Game: React.FC<GameProps> = ({
 
   const handleSpecialItemMatch = (count: number) => {
     if (gameState.isGameOver) return;
-<<<<<<< HEAD
     
-=======
->>>>>>> master
     console.log(`Special item matched ${count} times`);
     setGameState((prev: GameState) => ({
       ...prev,
